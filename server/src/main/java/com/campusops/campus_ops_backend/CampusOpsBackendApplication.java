584be9712a04cf80a6845f55e55dd633ee2be571
Evil-Shown <damitharaveesh@gmail.com>
2026-04-12 00:46:23 +0530

    Initialize project structure and add initial files

---
+++ server/src/main/java/com/campusops/campus_ops_backend/CampusOpsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campusops.campus_ops_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusOpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusOpsBackendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/campusops/campus_ops_backend/CampusOpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusops.campus_ops_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusOpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
