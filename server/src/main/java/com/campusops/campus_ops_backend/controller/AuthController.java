fd858ee5393959bfce4184db45b9d225712bb0ee
Nethshan Dulmin Bulegoda <dulminbulegoda@gmail.com>
2026-04-25 03:50:28 +0530

    configuration fix

--- server/src/main/java/com/campusops/campus_ops_backend/controller/AuthController.java
+++ server/src/main/java/com/campusops/campus_ops_backend/controller/AuthController.java
@@ -31,7 +31,7 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.signup(request));
     }
 
-    @PostMapping({ "/api/auth/signin", "/api/auth/login" })
+    @PostMapping("/api/auth/signin")
     public ResponseEntity<AuthResponseDTO> signin(@Valid @RequestBody SigninRequestDTO request) {
         return ResponseEntity.ok(authService.signin(request));
     }
