d29e0687b015751de42f2c523dd09387baa09bcf
Mithun <IT23656406@my.sliit.lk>
2026-04-26 00:10:44 +0530

    Modified ResourseRepo.jav file

--- server/src/main/java/com/campusops/campus_ops_backend/repository/ResourceRepository.java
+++ server/src/main/java/com/campusops/campus_ops_backend/repository/ResourceRepository.java
@@ -13,12 +13,26 @@ import com.campusops.campus_ops_backend.model.Resource;
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
     @Query("SELECT r FROM Resource r WHERE " +
-           "(:type IS NULL OR r.type = :type) AND " +
-           "(:location IS NULL OR LOWER(r.location) LIKE LOWER(CONCAT('%',:location,'%'))) AND " +
-           "(:minCapacity IS NULL OR r.capacity >= :minCapacity) AND " +
-           "r.status <> 'OUT_OF_SERVICE'")
+            "(CAST(:type AS string) IS NULL OR r.type = :type) AND " +
+            "(CAST(:location AS string) IS NULL OR LOWER(r.location) LIKE LOWER(CONCAT('%', CAST(:location AS string), '%'))) AND " +
+            "(CAST(:minCapacity AS integer) IS NULL OR r.capacity >= :minCapacity) AND " +
+            "r.status <> 'OUT_OF_SERVICE'")
     List<Resource> search(
-        @Param("type") Resource.ResourceType type,
-        @Param("location") String location,
-        @Param("minCapacity") Integer minCapacity);
+            @Param("type") Resource.ResourceType type,
+            @Param("location") String location,
+            @Param("minCapacity") Integer minCapacity);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
