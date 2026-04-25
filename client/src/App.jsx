08c0bd3524107d44c512ec5d3947e78b625373f5
Evil-Shown <damitharaveesh@gmail.com>
2026-04-26 03:11:15 +0530

    Route issue fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,6 @@ import ResourceDetailPage from './pages/resources/ResourceDetailPage'
 import BookingFormPage from './pages/bookings/BookingForm'
 import MyBookingsPage from './pages/bookings/MyBookings'
 import BookingDetailPage from './pages/bookings/BookingDetail'
-import TicketListPage from './pages/tickets/TicketListPage'
 import TicketCreatePage from './pages/tickets/TicketCreatePage'
 import TicketsPage from './pages/TicketsPage'
 import TicketDetailPage from './pages/TicketDetailPage'
@@ -46,7 +45,7 @@ function App() {
           <Route path="/bookings/new/:resourceId" element={withProtectedLayout(<BookingFormPage />)} />
           <Route path="/bookings/my" element={withProtectedLayout(<MyBookingsPage />)} />
           <Route path="/bookings/:id" element={withProtectedLayout(<BookingDetailPage />)} />
-          <Route path="/tickets" element={withProtectedLayout(<TicketListPage />)} />
+          <Route path="/tickets" element={withProtectedLayout(<TicketsPage />)} />
           <Route path="/tickets/new" element={withProtectedLayout(<TicketCreatePage />)} />
           <Route path="/tickets/:id" element={withProtectedLayout(<TicketDetailPage />)} />
           <Route path="/admin" element={withProtectedLayout(<AdminDashboardPage />, ['ADMIN'])} />
